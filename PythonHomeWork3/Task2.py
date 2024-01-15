6dc9a62236c18b6f5349202a191c416462b6314c
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-01-15 09:52:37 +0300

    V3

--- PythonHomeWork3/Task2.py
+++ PythonHomeWork3/Task2.py
@@ -1,7 +1,7 @@
 # Требуется найти в массиве list_1 самый близкий по значению элемент к заданному числу k и вывести его.
 # Считать, что такой элемент может быть только один. Если значение k совпадает с этим элементом - выведите его.
 
-list_1 = [1, 4, 3, 7, 8, 9, 2]
+list_1 = [2, 4, 1, 6, 8, 2, 9, 3, 2, 5]
 res = 0
 multiply = 0
 k = int(input("Введите число: "))
@@ -9,8 +9,10 @@ difference = k
 for i in list_1:
     if k - i < 0:
         multiply = (k - i) * -1
+    else:
+        multiply = k - i
     if multiply < difference:
-        difference = k - i
+        difference = multiply
         res = i
     elif k == i:
         res = i
