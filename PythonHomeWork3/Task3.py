3d151ff997af3ec49868a2821db13355a77ff00c
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-01-15 08:53:30 +0300

    V2

--- PythonHomeWork3/Task3.py
+++ PythonHomeWork3/Task3.py
@@ -21,25 +21,24 @@
 # Напишите программу, которая вычисляет стоимость введенного пользователем слова k и выводит его. Будем считать, 
 # что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.
 
-text = input().upper().split("")
+text = input()
+text = text.lower()
 sum = 0
 for i in text:
-    if i == "А" or i == "В" or i == "Е" or i == "И" or i == "Н" or i == "О" or i == "Р" or i == "С":
+    if i == "а" or i == "в" or i == "е" or i == "и" or i == "н" or i == "о" or i == "р" or i == "с" or i == "т":
         sum += 1
-    elif i == "Т" or i == "A" or i == "E" or i == "I" or i == "O":
+    if i == "a" or i== "e" or i == "i" or i == "o" or i == "u" or i == "l" or i == "n" or i == "s" or i == "t" or i == "r":
         sum += 1
-    elif i == "U" or i == "L" or i == "N" or i == "S" or i == "T" or i == "R":
-        sum += 1
-    elif i == "Д" or i == "К" or i == "Л" or i == "М" or i == "П" or i == "У" or i == "D" or i == "G":
+    if i == "d" or i == "g" or i == "д" or i == "к" or i == "л" or i == "м" or i == "п" or i == "у":
         sum += 2
-    elif i == "Б" or i == "Г" or i == "Ё" or i == "Ь" or i == "Я" or i == "B" or i == "C" or i == "M" or i == "P":
+    if i == "b" or i == "c" or i == "m" or i == "p" or i == "б" or i == "г" or i == "ё" or i == "ь" or i == "я":
         sum += 3
-    elif i == "Й" or i == "Ы" or i == "F" or i == "H" or i == "V" or i == "W" or i == "Y":
+    if i == "f" or i == "h" or i == "v" or i == "w" or i == "y" or i == "й" or i == "ы":
         sum += 4
-    elif i == "Ж" or i == "З" or i == "Х" or i == "Ц" or i == "Ч" or i == "K":
+    if i == "k" or i == "ж" or i == "з" or i == "х" or i == "ц" or i == "ч":
         sum += 5
-    elif i == "Ш" or i == "Э" or i == "Ю" or i == "J" or i == "X":
+    if i == "ш" or i == "э" or i == "ю" or i == "j" or i == "x":
         sum += 8
-    elif i == "Ф" or i == "Щ" or i == "Ъ" or i == "Q" or i == "Z":
+    if i == "q" or i == "z" or i == "ф" or i == "щ" or i == "ъ":
         sum += 10
 print(sum)
